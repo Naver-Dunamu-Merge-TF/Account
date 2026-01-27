33c15cf6e6ec122c63e19e17ba768aa8b60f014b
kanghyoseung <kanghyoseung@gmail.com>
2026-01-27 15:29:43 +0900

    init : add initial setting

---
+++ src/main/java/com/example/cryptoorder/CryptoOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoOrderApplication.class, args);
+    }
+
+}
